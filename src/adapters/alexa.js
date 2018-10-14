85eacf49f18722cd218f6a65131ca178009f8fec
Wade Penistone <wadespackages@yahoo.com>
2018-10-14 13:23:10 +0100

    Added alexa adapter

---
+++ src/adapters/alexa.js
@@ -0,0 +1,9 @@
+module.exports = {
+    input: {
+        parser: 'none'
+    },
+    output: {
+        format: 'ssml',
+        target: 'device'
+    }
+};
--- test.js
+++ test.js
@@ -1,8 +1,11 @@
 const IntentClassifier = require('molir/molir');
 const LowBot = require('./src/index');
+const alexa = require('./src/adapters/alexa');
 const discord = require('./src/adapters/discord');
 const terminal = require('./src/adapters/terminal');
 const intents = require('./build/intents.json').intents;
 
-let botInstance = new LowBot({discord, terminal}, intents, IntentClassifier);
-botInstance.respond('hello', 'terminal');
+let botInstance = new LowBot({
+    alexa, discord, terminal
+}, intents, IntentClassifier);
+botInstance.respond('hello', 'alexa');
