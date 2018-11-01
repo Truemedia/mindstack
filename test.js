b30b4470521f4d70774176cc55a43eed0dece031
Wade Penistone <wadespackages@yahoo.com>
2018-11-01 20:37:35 +0000

    Adapters folder deprecated

--- test.js
+++ test.js
@@ -1,15 +1,25 @@
-const IntentClassification = require('eduir');
+// Core
 const LowBot = require('./src/index');
+
+// Classifier
+const IntentClassification = require('eduir');
+
+// Adapters
 const alexa = require('lowbot-alexa');
 const discord = require('lowbot-discord');
-const terminal = require('./src/adapters/terminal');
+const slack = require('lowbot-slack');
+const terminal = require('lowbot-terminal');
+
+// Skills
 const intents = require('./build/intents.json').intents;
 const BasicSkill = require('./src/skills/basic/index');
 
+// Bot instance
 let botInstance = new LowBot()
   .applyClassifier(IntentClassification, intents)
-  .useAdapter(discord)
   // .useAdapter(alexa)
+  .useAdapter(discord)
+  // .useAdapter(slack)
   // .useAdapter(terminal)
   .addSkill(BasicSkill) // Add our only skill at the moment
   .enableDataService() // Allow skills to use the data service specified in .env
